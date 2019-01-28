5b3ee31c587088965551165e9668a7a109ff9256
Shinyn <mattias.alm@hotmail.com>
2019-01-29 00:53:08 +0100

    Uppdaterat Activity Klassen

---
+++ app/src/main/java/com/example/matti/whatnow/EditActivity.java
@@ -0,0 +1,22 @@
+package com.example.matti.whatnow;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+
+        Intent intent = getIntent();
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/matti/whatnow/MainActivity.java
+++ app/src/main/java/com/example/matti/whatnow/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.matti.whatnow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MESSAGE = "imageID";
+
+    ImageView whatNow;
+    Intent intent;
+    Activity userActivity = new Activity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        intent = new Intent(this, RandomActivity.class);
+        whatNow = findViewById(R.id.whatNow);
+        whatNow.setImageResource(R.drawable.whatnow);
+        whatNow.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getActionMasked();
+                switch (action) {
+                    case (MotionEvent.ACTION_UP) :
+                    userActivity.generate();
+                return true;
+                    case (MotionEvent.ACTION_DOWN) :
+                        return true;
+                }
+                return false;
+            }
+        });
+
+//        Activity newActivity = new Activity("bowl", "sunday", "jeff");
+//        addToList(newActivity);
+//        Activity newActivity2 = new Activity("plow", "2 hours", "you mom");
+//        addToList(newActivity2);
+
+//        Button button = (Button) findViewById(R.id.edit);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//            }
+//        });
+
+        //readFromList();
+
+
+
     }
+
+//    public void addToList(Activity activityToAdd) {
+//        activitySuggestion.add(activityToAdd);
+//    }
+//
+//    public Activity readFromList() {
+//        int index = randomizer.nextInt(activitySuggestion.size());
+//        Activity item = activitySuggestion.get(index);
+//        System.out.println("You should " + item.getName() + " with " + item.getParticipants() +
+//                " for " + item.getDuration());
+//        return item;
+//    }
+//
+//    public void whtNowButtonPressed(View view) {
+//        intent.putExtra(MESSAGE, R.drawable.whatnow);
+//        startActivity(intent);
+//    }
+
+
 }
