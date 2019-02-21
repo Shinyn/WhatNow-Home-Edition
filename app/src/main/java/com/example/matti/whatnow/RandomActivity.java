721afca7fac0ab998a04eee7c279d2d1a7dcd7e3
Shinyn <mattias.alm@hotmail.com>
2019-02-21 14:46:36 +0100

    RandomView's whatNow button can be spammed and has fixed position

--- app/src/main/java/com/example/matti/whatnow/RandomActivity.java
+++ app/src/main/java/com/example/matti/whatnow/RandomActivity.java
@@ -2,9 +2,12 @@ package com.example.matti.whatnow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class RandomActivity extends AppCompatActivity {
+
+    StuffToDo ustd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +16,20 @@ public class RandomActivity extends AppCompatActivity {
 
         // Tar in extra från main och säger att den vi tar in är en StuffToDo och lägger den i en
         // ny StuffToDo
-        StuffToDo ustd = (StuffToDo) getIntent().getSerializableExtra("list");
+        ustd = (StuffToDo) getIntent().getSerializableExtra("list");
 
+        TextView suggestedActivity = findViewById(R.id.suggestedActivity);
+        suggestedActivity.setText(ustd.generateName());
+
+        TextView suggestedParticipants = findViewById(R.id.participants);
+        suggestedParticipants.setText(ustd.generateParticipants());
+
+        TextView suggestedDuration = findViewById(R.id.actualDuration);
+        suggestedDuration.setText(ustd.generateDuration());
+
+    }
+
+    public void refreshPage(View v) {
         TextView suggestedActivity = findViewById(R.id.suggestedActivity);
         suggestedActivity.setText(ustd.generateName());
 
